7150862381627bf9feaa255f49f0293e123bf362
vinhne <phamquocvinh99@gmail.com>
2015-06-08 11:47:05 +0700

    chang url image

--- frontend/protected/config/main.php
+++ frontend/protected/config/main.php
@@ -86,7 +86,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-        'base_url_image' => 'http://local.aj-bet/frontend/assets/team/',
+        'base_url_image' => 'http://api.betskeez.com/frontend/assets/team/',
 		'adminEmail' => 'webmaster@example.com',
         'db_prefix' => 'skeez_',
         'php_mailer' => array(
